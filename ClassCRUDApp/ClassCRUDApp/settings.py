71dd0d4be8f65102c497d7e56fb01ac0443a145f
Code With Hindi <123615985+CodeWithHindi@users.noreply.github.com>
2023-05-26 01:41:41 +0530

    Update settings.py

--- ClassCRUDApp/ClassCRUDApp/settings.py
+++ ClassCRUDApp/ClassCRUDApp/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+y9fwo*g+8*ay4yu29qm10_)kv7%k67jj154$fow!q-*ny630'
+SECRET_KEY = 'create_your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
